2f6ccfbfb01dfb8409b2afc92c8a1cf975bbbb91
Kundan <61063959+kundan-de@users.noreply.github.com>
2025-09-16 12:09:35 +0000

    chore: added download button

--- app.py
+++ app.py
@@ -11,12 +11,18 @@ def main():
         "Upload an Image", type=["png", "jpg", "jpeg"], key="uploader"
     )
     if uploaded_file is not None:
-        remove_bg(uploaded_file)
+        img = remove_bg(uploaded_file)
         st.success("Background removed successfully!")
-        st.image(tempfile.gettempdir() + "/output.png", caption="Processed Image")
+        st.image(img, caption="Processed Image")
+        st.download_button(
+            label="Download Image", data=img, file_name="output.png", mime="image/png"
+        )
+    else:
+        st.info("Please upload an image to remove its background.")
 
 
 def remove_bg(input_path: str):
+    """Remove background from the image and save the output."""
     input = Image.open(input_path)
     output = remove(input)
     output.save(tempfile.gettempdir() + "/output.png")
